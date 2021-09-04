aa52e15d229bf3822c5b3b7ffb0d5b851c348b97
Borna Morassaei <a1745552@student.adelaide.edu.au>
2021-09-04 13:04:47 +0930

    Added code to view the teachers weekly timetable

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   post "admins" => "admins#set_restriction_level"
   get "clashes" => "clashes#index"
   get "week"    => "weeklies#index"
+  get "teachers"=> "teachers#index"
+  get "teachers/weekly/:id" => "teachers#weekly"
 end
