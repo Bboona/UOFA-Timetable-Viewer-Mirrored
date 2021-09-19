fe3d33fcd804361addaa084e28b73d5696556b70
a1686874 <a1686874@student.adelaide.edu.au>
2021-09-19 20:15:21 +0930

    create admin migration file

---
+++ db/migrate/20210914014959_create_admins.rb
@@ -0,0 +1,11 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+      t.text :uni_id
+      t.text :first_name
+      t.text :last_name
+      t.text :password
+      t.timestamps
+    end
+  end
+end
