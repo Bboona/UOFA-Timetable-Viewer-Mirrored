65b8df5766b6996651ad9606f900a36ae0e701ce
Borna Morassaei <a1745552@student.adelaide.edu.au>
2021-09-26 00:27:55 +0930

    Fixed weekly timetable views, added clash lists, also added weekly timetable views for students

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -16,12 +16,36 @@ class TeachersController < ApplicationController
     @weekly = Weekly.where(:id => params[:id]).first
     week_bit = @weekly.week.to_i
     @activities = Teacher.first.activities
-    @this_week =[]
+    @this_week = Array.new(16){Array.new(7,1)}
+    @time_names = ["9:00 am","","10:00 am","","11:00 am","","12:00 pm","","1:00 pm","","2:00 pm","","3:00 pm","","4:00 pm","",]
+    @day_bits = [1,2,4,8,16,32,64]
     @hours = [65536,131072,262144,524288,1048576,2097152,4194304,8388608,16777216,33554432,67108864,134217728,268435456,536870912,1073741824,2147483648]
-    @activities.each do |activity|
-      activity_week_bit = activity.weeks.to_i
-      if activity_week_bit&week_bit==week_bit &&
-        @this_week << activity
+    @act_details = []
+    @clashes = []
+    @counter = 0
+    @day_bits.each_with_index do |day,i|
+      @hours.each_with_index do |time,j|
+        @activities.each do |act|
+          if act.weeks.to_i & week_bit == week_bit && act.hours.to_i & time == time && act.days.to_i & day == day && @act_details.exclude?(act)
+            @counter=@counter+1
+            if @this_week[j][i] == 1
+              @act_details << act
+              @this_week[j][i] = act
+              @hamming_temp = act.hamming_weight
+              for n in 1..@hamming_temp.to_i-1 do
+                if j+n < 16
+                  @this_week[j+n][i] = 0
+                else
+                  break
+                end
+              end
+            else
+              if @clashes.exclude?(act)
+                @clashes << act
+              end
+            end
+          end
+        end
       end
     end
 
