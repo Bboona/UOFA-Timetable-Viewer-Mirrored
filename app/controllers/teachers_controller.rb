af9bbbb0b8b4b6dda68db460c769f77aa5c2a5ed
Michael Emilio Daniele <michael.daniele@student.adelaide.edu.au>
2021-09-27 10:49:02 +0930

    Update teachers_controller.rb

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -117,6 +117,7 @@ class TeachersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_teacher
+      @teacher = Teacher.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
