7f0223f17b9225f942f56b68751ae6895f97d9b6
Damian Suhaj <a1747931@student.adelaide.edu.au>
2021-10-27 19:48:33 +1030

    change database stuff with new master

--- db/migrate/20210910013647_create_activities_students.rb
+++ db/migrate/20210910013647_create_activities_students.rb
@@ -2,7 +2,7 @@ class CreateActivitiesStudents < ActiveRecord::Migration[6.1]
   def change
     create_join_table :activities, :students do |t|
       t.index :activity_id
-      t.index :uni_id
+      t.string :uni_id
     end
   end
 end
